20a01887027f0ab010622001081007f37cc0cae7
Alexey <77489413+BaldiSlayer@users.noreply.github.com>
2023-05-08 22:35:36 +0300

    first commit

---
+++ Server.py
@@ -0,0 +1,149 @@
+import requests
+from bs4 import BeautifulSoup
+
+LANG_TO_ID = {
+    'C': 1,
+    'Scheme': 2,
+    'Go': 3,
+    'Java': 4,
+    'C++': 5,
+    'Markdown': 6,
+}
+
+STRING_TO_VERDICT = {
+    'Тесты пройдены': 'passed',
+    'Отклонено преподавателем': 'banned',
+    'Зачтено': 'accepted',
+    'Тесты не пройдены': 'failed',
+    'Ожидает проверки': 'waiting',
+}
+
+class ServerConnection:
+    def __init__(self, login='22u801', password='vtsz9lko'):
+        self.login = login
+        self.password = password
+
+        self.session = requests.Session()
+
+        r = self.session.post('https://hw.iu9.bmstu.ru/auth',
+                              data={
+                                  'login': self.login,
+                                  'password': self.password,
+                              })
+
+        self.status = r.status_code
+
+    def print_text(self):
+        print(self.session.get('https://hw.iu9.bmstu.ru/student').text)
+
+    def get_subjects(self):
+        response = self.session.get('https://hw.iu9.bmstu.ru/student?course_id=-200')
+        html = response.content
+
+        # создаем объект BeautifulSoup
+        soup = BeautifulSoup(html, 'html.parser')
+
+        elements = soup.find_all('div', class_='header_menu-element')
+
+        answer = []
+
+        for element in elements:
+            a = {
+                'link': element.find('a')['href'].replace('/student?course_id=', ''),
+                'text': element.text.strip(),
+            }
+            answer.append(a)
+
+        return answer
+
+    def get_course_tasks(self, course_id: int) -> str:
+        response = self.session.get(f'https://hw.iu9.bmstu.ru/student?course_id={course_id}')
+        html = response.content
+
+        # создаем объект BeautifulSoup
+        soup = BeautifulSoup(html, 'html.parser')
+
+        table = soup.find('table', {'class': 'tasks_table'})
+
+        answer = {}
+
+        rows = table.find_all('tr')
+
+        lst = []
+        num = 1
+        for row in rows:
+            cols = row.find_all('td')
+            to_print = row.text.strip().replace('\n', ' ').replace('\t', '').split()
+
+            if to_print[0] == 'Модуль' or to_print[0] == 'Сумма':
+                if lst:
+                    answer[f'Модуль {num}'] = lst
+                    num += 1
+                    lst = []
+                continue
+            if len(cols[1].attrs['class']) > 1:
+                verdict = cols[1].attrs['class'][1].replace('score_', '')
+            else:
+                verdict = 'nonsolved'
+            lst.append(
+                {
+                    'name': to_print[0],
+                    'degree': to_print[1],
+                    'max': to_print[2],
+                    'verdict': verdict,
+                    'link': row.find_all('td')[0].find('a')['href'].replace('/submission?task_id=', ''),
+                }
+            )
+
+        return answer
+
+    def get_task_by_id(self, task_id):
+        response = self.session.get(f'https://hw.iu9.bmstu.ru/submission?task_id={task_id}')
+        html = response.content
+
+        # создаем объект BeautifulSoup
+        soup = BeautifulSoup(html, 'html.parser')
+
+        # получаем условие задачи
+        task = soup.find('div', {'class': 'paper content'}).find('div')
+
+        # получаем ЯП, на которых ее можно сдать
+        labels = soup.find_all('label')
+
+        languages = []
+        for label in labels:
+            if label.attrs['for'].startswith('lang'):
+                languages.append(label.text)
+
+        submissions = []
+        # получаем попытки решения
+        tries = soup.find_all('details', {'class': 'test-result_details'})
+        for tri in tries:
+            summary_text = tri.summary.text.strip().split()
+            submissions.append(
+                {
+                    'date': summary_text[1] + " " + summary_text[2],
+                    'verdict': STRING_TO_VERDICT[' '.join(summary_text[3:])],
+                    'id': len(submissions),
+                    'lang': tri.find('div').text.split('\n')[1].replace('Язык реализации: ', ''),
+                }
+            )
+
+        return task, languages, submissions
+
+    def sumbit_solution(self, task_id, code, language):
+        r = self.session.post(f' https://hw.iu9.bmstu.ru/submission?task_id={task_id}',
+                          data={
+                              'lang': LANG_TO_ID[language],
+                              'submission': code,
+                          })
+        return r.status_code
+
+    def get_code(self, task_id, code_id):
+        response = self.session.get(f'https://hw.iu9.bmstu.ru/submission?task_id={task_id}')
+        html = response.content
+
+        # создаем объект BeautifulSoup
+        soup = BeautifulSoup(html, 'html.parser')
+
+        return str(soup.find_all('details', {'class': 'test-result_paragraph'})[code_id*2].find('div').find('div'))
---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from Server import ServerConnection
+from markupsafe import Markup
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def login_page():
+    return render_template("login.html")
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+
+    global server
+
+    # TODO PASTE PARAMETRES
+    server = ServerConnection()
+
+    if server.status >= 300:
+        return jsonify({'message': 'error'})
+
+    return redirect(url_for('home'))
+
+
+@app.route('/home')
+def home():
+    return render_template('courses.html', data=server.get_subjects())
+
+
+@app.route('/course/<int:course_id>')
+def course(course_id):
+    return render_template('contest.html', content=server.get_course_tasks(course_id), subject='АиСД')
+
+@app.route('/task/<int:task_id>')
+def task(task_id):
+    statement, languages, submissions = server.get_task_by_id(task_id)
+    return render_template('task.html', statement=Markup(statement), languages=languages, submissions=submissions)
+
+@app.route('/task/<int:task_id>/submit', methods=['POST'])
+def submit(task_id):
+    data = request.get_json()
+    code = data['code']
+    language = data['language']
+
+    return server.sumbit_solution(task_id, code, language)
+
+@app.route('/task/<int:task_id>/submission/<int:code_id>')
+def get_code(task_id, code_id):
+    return server.get_code(task_id, code_id)
+
+
+if __name__ == '__main__':
+    app.run()
